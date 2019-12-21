983d22ad06d96ab228cc9f80da2419350c54537a
Pedro Barata <pedro.barata@fasolti.com.br>
2019-12-20 21:48:29 -0300

    feat(recipe list, recipe model): Recipe list and model created

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [new Recipe('New recipe', 'Description of Recipe', 'https://www.inspiredtaste.net/wp-content/uploads/2018/12/Sauteed-Zucchini-Recipe-1-1200.jpg')]
   constructor() { }
 
   ngOnInit() {
